473025d58705ae6b708172433290df0a712e0328
igorsants <99425352+igorsants@users.noreply.github.com>
2023-09-25 19:49:54 -0300

    Create cliente.java

---
+++ atividade6/cliente.java
@@ -0,0 +1,35 @@
+public class Cliente {
+    private String nome;
+    private String endereco;
+    private String identificacao;
+
+    public Cliente(String nome, String endereco, String identificacao) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.identificacao = identificacao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getIdentificacao() {
+        return identificacao;
+    }
+
+    public void setIdentificacao(String identificacao) {
+        this.identificacao = identificacao;
+    }
+}
